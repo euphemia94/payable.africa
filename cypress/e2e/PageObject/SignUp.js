3a2e567eda6b86473696940acfb44b7a47fc52f4
euphemia94 <euphemiauc94@gmail.com>
2023-07-18 11:11:57 -0700

    commit new changes

--- cypress/e2e/PageObject/SignUp.js
+++ cypress/e2e/PageObject/SignUp.js
@@ -1,17 +1,18 @@
 const xpath = require('cypress-xpath');
+
 export class SignUp {
     visit(){
         cy.visit('https://staging.payable.africa/');
     }
 
     signUpForm(firstName, lastName, email, passward){
-        cy.xpath('//*[@id=":r10:"]').click();
-        cy.xpath('//*[@id=":r14:"]').type(firstName);
-        cy.xpath('//*[@id=":r15:"]').type(lastName);
-        cy.xpath('//*[@id=":r16:"]').type(email);
-        cy.xpath('//*[@id=":r17:"]').type(passward);
+        cy.xpath('//*[@id=":r4:"]').click();
+        cy.xpath('//*[@id=":rm:"]').type(firstName);
+        cy.xpath('//*[@id=":rn:"]').type(lastName);
+        cy.xpath('//*[@id=":ro:"]').type(email);
+        cy.xpath('//*[@id=":rp:"]').type(passward);
         cy.xpath('//*[@id="root"]/div[1]/main/div/div/div[2]/form/div/div[9]/label/span[1]/input').click();
-        cy.xpath('//*[@id=":r18:"]').click();
+        cy.xpath('//*[@id=":rq:"]').click();
     }
 
     verifyEmail(code){
